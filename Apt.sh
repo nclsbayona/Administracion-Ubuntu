c02b318b9987b4db72e867c60e02630513586808
Nicolas Bayona <nbayonaord@gmail.com>
2021-11-29 15:15:40 -0500

    Updating Apt.sh

--- Apt.sh
+++ Apt.sh
@@ -7,8 +7,8 @@ if [[ "${UID}" -eq 0 ]]
         #Check everything is ok, and do cleaning
 	printf '\n%s\n' 'Checking and cleaning'        
         sudo apt-get check
-	sudo apt-get purge --auto-remove
-	sudo apt-get -y autoremove --purge
+	    sudo apt-get purge -y --auto-remove
+	    sudo apt-get -y autoremove --purge
         sudo apt-get -y clean
         sudo apt-get -y autoclean
 	printf '\n%s\n' 'Ram-cache and Swap'
