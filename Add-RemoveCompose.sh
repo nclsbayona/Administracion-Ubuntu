fea2188536bf1f7aeccfe8a8d68b0c34823ea948
nclsbayona <59931437+nclsbayona@users.noreply.github.com>
2022-07-05 18:05:44 -0500

    Update Add-RemoveCompose.sh

--- Add-RemoveCompose.sh
+++ Add-RemoveCompose.sh
@@ -1,4 +1,4 @@
-# For version 1.29.2, for different version change the version tag
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod 777 /usr/local/bin/docker-compose
+VERSION=2.6.1
+sudo curl -L "https://github.com/docker/compose/releases/download/v$VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod 777 /usr/local/bin/docker-compose
 #To remove
 # sudo rm /usr/local/bin/docker-compose
